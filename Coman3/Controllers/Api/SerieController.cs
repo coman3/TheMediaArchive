b1eb7e30d505b64248f17d73ddac1a9e8de853a3
Lachlan van der Velden <lvelden1@gmail.com>
2016-05-23 10:21:41 +1000

    Config Help Page to output XmlDocs

--- Coman3/Controllers/Api/SerieController.cs
+++ Coman3/Controllers/Api/SerieController.cs
@@ -23,6 +23,11 @@ namespace Coman3.Controllers.Api
             _serieHelper = new SerieHelper(_dbContext);
         }
         
+        /// <summary>
+        /// Get A Paged List of <see cref="Serie"/>'s with all infomation about the serie, seasons and episodes
+        /// </summary>
+        /// <param name="bag">The Colection Modifyer Bag, can be collected from /Series/Index/ </param>
+        /// <returns>A List of "Bag.ItemsPerPage" <see cref="Serie"/>'s, with all data acociated.</returns>
         public List<Serie> Get([FromUri] SerieIndexBag bag)
         {
             return _serieHelper.GetSeriesFromQuery(bag).ToPagedList(bag.Page, bag.ItemsPerPage).ToList();
